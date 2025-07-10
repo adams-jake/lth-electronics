15f2709ed8245aff1eaea4bbed9ff63fccb59127
Jake Adams <info@jake-adams.com>
2025-07-10 17:04:56 +0100

    + introduction module

--- httpdocs/wp-content/themes/lth-electronics/modules/introduction.php
+++ httpdocs/wp-content/themes/lth-electronics/modules/introduction.php
@@ -3,6 +3,12 @@
     $title = $data['title'] ?? '';
     $content = $data['content'] ?? '';
 
+    // Image
+    $image = new Image($data['image'] ?? ''); 
+
+    // Link
+    $link = new Link($data['link']);
+    
     // Options    
     $backgroundColour = $data['background_colour'] ?? '';
     $spacing =  module_row_spacing($backgroundColour);
@@ -10,12 +16,32 @@
 ?>
 
 <section class="<?php echo classlist('intro', $backgroundColour, $spacing, $textColour); ?>">
-    <div class="page-width">
-        <div class="intro__inner first-last">
-            <h2 class="heading-2"><?php echo $title; ?></h2>
-            <div class="text-main first-last">
-                <?php echo $content; ?>
+    <div class="page-width--large">
+        <p class="label">About us</p>
+        <div class="grid grid--gutter flex-space-between">
+            <div class="col-14@medium">
+                <div class="intro__inner first-last">
+                    <h2 class="heading-2"><?php echo $title; ?></h2>
+                    <div class="text-main first-last">
+                        <?php echo $content; ?>
+                    </div>
+                    <?php echo renderButton($link); ?>
+                </div>
+            </div>
+            <div class="col-7@medium">
+                <?php if ($image->hasImage()) : ?>
+                    <img 
+                        class="object-fit border-radius" 
+                        src="<?php echo $image->src ?>"
+                        srcset="<?php echo $image->srcset ?>"
+                        sizes="(min-width: 62em) 50vw, 100vw"
+                        role="presentation" 
+                        alt="<?php echo $image->alt ?>"
+                        style="<?php echo $image->style ?>"
+                    >
+                <?php endif ?>
             </div>
         </div>
+        
     </div>
 </section>
