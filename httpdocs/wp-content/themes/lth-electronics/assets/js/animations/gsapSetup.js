7ce7285e6327995152427bc864b7d77075d380b0
Jake Adams <info@jake-adams.com>
2025-07-29 14:34:27 +0100

    …

--- httpdocs/wp-content/themes/lth-electronics/assets/js/animations/gsapSetup.js
+++ httpdocs/wp-content/themes/lth-electronics/assets/js/animations/gsapSetup.js
@@ -1,6 +1,6 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import SplitText from "gsap/splitText";
+import SplitText from "gsap/SplitText";
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
