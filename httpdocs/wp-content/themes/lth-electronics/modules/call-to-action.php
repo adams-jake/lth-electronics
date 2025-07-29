0d33edbba6aab04e940403df22cda1129fc3a408
Jake Adams <info@jake-adams.com>
2025-07-29 08:56:06 +0100

    …

--- httpdocs/wp-content/themes/lth-electronics/modules/call-to-action.php
+++ httpdocs/wp-content/themes/lth-electronics/modules/call-to-action.php
@@ -3,24 +3,53 @@
     $title = $data['title'] ?? '';
     $content = $data['content'] ?? '';
 
+    // Image
+    $image = new Image($data['image'] ?? '');     
+
     // Link
     $link = new Link($data['link']);
 
     // Options    
     $backgroundColour = $data['background_colour'] ?? '';
     $textColour = module_text_color($backgroundColour);
+
+    // Decoration
+    $lineTotal = 7;
 ?>
 
-<section class="call-to-action margin-y">
+<section class="call-to-action bg--white padding-y load">
     <div class="page-width--narrow">
-        <div class="<?php echo classlist('first-last box-padding', $backgroundColour, $textColour); ?>">
-            <h2 class="heading-2"><?php echo $title; ?></h2>
-            <div class="text-main">
-                <?php echo $content; ?>
+        <div class="<?php echo classlist('bg--blue text-white border-radius', $backgroundColour); ?>">
+            <div class="grid">
+                <div class="call-to-action__inner col-16@medium relative">
+                    <div class="call-to-action__content first-last" style="padding: 3em;">
+                        <h2 class="heading-2"><?php echo $title; ?></h2>
+                        <div class="text-main">
+                            <?php echo $content; ?>
+                        </div>
+                        <?php echo renderButton($link); ?>
+                    </div>
+                    <div class="call-to-action__circle" style="--cta-line-total: <?php echo $lineTotal; ?>">
+                        <?php foreach (range(1, $lineTotal) as $i) : ?>
+                              <div class="call-to-action__line" style="--cta-line-number: <?php echo $i; ?>"></div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+                <div class="col-8@medium relative hide show@medium" style="background: rgba(0,0,0,0.3);">
+                    <?php if ($image->hasImage()) : ?>
+                        <img 
+                            class="object-fit--absolute" 
+                            src="<?php echo $image->src ?>"
+                            srcset="<?php echo $image->srcset ?>"
+                            sizes="(min-width: 62em) 50vw, 100vw"
+                            role="presentation" 
+                            alt="<?php echo $image->alt ?>"
+                            style="<?php echo $image->style ?>"
+                        >
+                    <?php endif ?>
+                </div>
             </div>
-            <?php if ($link->hasLink()) : ?>
-                <a class="button" <?php echo $link->attributes; ?>><?php echo $link->text; ?></a>
-            <?php endif; ?>
+            
         </div>
     </div>
 </section>
